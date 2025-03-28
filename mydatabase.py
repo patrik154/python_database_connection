b8df0994203e0f7b79030db13b4647752e7f86e0
patrik154 <sinkopatrik@gmail.com>
2025-03-28 13:21:37 +0100

    kommentek hozzáadása

---
+++ mydatabase.py
@@ -0,0 +1,32 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="mysql"
+)
+
+mycursor = mydb.cursor()
+
+# adatbázis létrehozása
+DATABASE = "mydatabase"
+mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {DATABASE}")
+
+#adatbázisok mutatása
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x)
+
+# használd ezt az adatbázist
+mycursor.execute(f"USE {DATABASE}")
+
+# customers tábla létrehozása
+mycursor.execute("CREATE TABLE IF NOT EXISTS customers (name VARCHAR(255), address VARCHAR(255))")
+print("---------")
+
+# táblák mutatása
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+  print(x)
